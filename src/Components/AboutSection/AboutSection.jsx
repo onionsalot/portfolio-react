a7ddb510ee7c0ad558ea52880c0ef7394f66bb29
onionsalot <trong.nguyen1008@gmail.com>
2021-07-10 04:06:31 -0400

    dynamically renders top and bot section of about and populates with icons

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -14,64 +14,215 @@ import IconReact from '../../images/icons/react.png';
 import IconSass from '../../images/icons/sass.png';
 
 export default function AboutSection() {
-    const parallaxData0 = [
-        {
-            start:'.halfpoint',
-            end:'.endpoint',
-            properties: [
-                {
-                    startValue:100,
-                    endValue:0,
-                    property:'translateX',
-                }
-            ]
-        }, 
-        {
-            start: '.halfpoint',
-            end:'.endpoint',
-            properties: [
-                {
-                    startValue:0,
-                    endValue:1,
-                    property:'opacity'
-                }
-            ]
-        }
-        
-    ]
-    const parallaxData1 = [
-        {
-            start:'.halfpoint',
-            end:'.endpoint',
-            properties: [
-                {
-                    startValue:400,
-                    endValue:200,
-                    property:'translateX',
-                }
-            ]
-        }, 
-        {
-            start: '.halfpoint',
-            end:'.endpoint',
-            properties: [
-                {
-                    startValue:0,
-                    endValue:1,
-                    property:'opacity'
-                }
-            ]
-        }
-    ]
-
-    function comvertVWToPx(value) {
-        var parts = value.match(/([0-9\.]+)(vh|vw)/)
+    const [parallaxDataTop, setParallaxDataTop] = useState([]);
+    const [parallaxDataBot, setParallaxDataBot] = useState([]);
+    const [distance, setDistance] = useState(convertVWToPx('100vw')*0.8/5)
+    const [iconTop, setIconTop] = useState([ IconCss, IconDj, IconGit, IconHtml, IconJs, IconMongo ])
+    const [iconBot, setIconBot] = useState([ IconNodejs, IconPostgresql, IconPython, IconReact, IconSass ])
+    function convertVWToPx(value) {
+        var parts = value.match(/([0-9\.]+)(vh|vw)/) 
         var q = Number(parts[1])
         var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(parts[2])]]
         return side * (q/100)
       }
 
-    console.log(comvertVWToPx('100vw'))
+    // console.log((convertVWToPx('100vw')*0.8/5))
+
+    useEffect(() => {
+
+        const data = []
+        for (let i=0; i<5; i++) {
+            data.push(
+                [
+                    {
+                        start:'.parallax-top',
+                        end:'.halfpoint',
+                        properties: [
+                            {
+                                startValue:i*distance+100,
+                                endValue:i*distance,
+                                property:'translateX',
+                            }
+                        ]
+                    }, 
+                    {
+                        start: '.parallax-top',
+                        end:'.halfpoint',
+                        properties: [
+                            {
+                                startValue:0,
+                                endValue:1,
+                                property:'opacity'
+                            }
+                        ]
+                    }
+                ]
+            )
+        }
+        const data1 = []
+        for (let i=0; i<5; i++) {
+            data1.push(
+                [
+                    {
+                        start:'.halfpoint',
+                        end:'.endpoint',
+                        properties: [
+                            {
+                                startValue:i*distance-100,
+                                endValue:i*distance,
+                                property:'translateX',
+                            }
+                        ]
+                    }, 
+                    {
+                        start: '.halfpoint',
+                        end:'.endpoint',
+                        properties: [
+                            {
+                                startValue:0,
+                                endValue:1,
+                                property:'opacity'
+                            }
+                        ]
+                    }
+                ]
+            )
+        }
+        setParallaxDataTop(data)
+        setParallaxDataBot(data1)
+        // console.log(parallaxData)
+    }, [distance])
+
+
+    window.onresize = function() {
+        const newDistance = convertVWToPx('100vw')*0.8/5
+        if (newDistance !== distance) {
+            setDistance(newDistance)
+        }
+    }
+
+
+    // const parallaxData0 = [
+    //     {
+    //         start:'.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:100,
+    //                 endValue:0,
+    //                 property:'translateX',
+    //             }
+    //         ]
+    //     }, 
+    //     {
+    //         start: '.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:0,
+    //                 endValue:1,
+    //                 property:'opacity'
+    //             }
+    //         ]
+    //     }
+        
+    // ]
+    // const parallaxData1 = [
+    //     {
+    //         start:'.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:400,
+    //                 endValue:200,
+    //                 property:'translateX',
+    //             }
+    //         ]
+    //     }, 
+    //     {
+    //         start: '.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:0,
+    //                 endValue:1,
+    //                 property:'opacity'
+    //             }
+    //         ]
+    //     }
+    // ]
+    // const parallaxData2 = [
+    //     {
+    //         start:'.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:600,
+    //                 endValue:400,
+    //                 property:'translateX',
+    //             }
+    //         ]
+    //     }, 
+    //     {
+    //         start: '.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:0,
+    //                 endValue:1,
+    //                 property:'opacity'
+    //             }
+    //         ]
+    //     }
+    // ]
+    // const parallaxData3 = [
+    //     {
+    //         start:'.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:800,
+    //                 endValue:600,
+    //                 property:'translateX',
+    //             }
+    //         ]
+    //     }, 
+    //     {
+    //         start: '.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:0,
+    //                 endValue:1,
+    //                 property:'opacity'
+    //             }
+    //         ]
+    //     }
+    // ]
+    // const parallaxData4 = [
+    //     {
+    //         start:'.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:1000,
+    //                 endValue:800,
+    //                 property:'translateX',
+    //             }
+    //         ]
+    //     }, 
+    //     {
+    //         start: '.halfpoint',
+    //         end:'.endpoint',
+    //         properties: [
+    //             {
+    //                 startValue:0,
+    //                 endValue:1,
+    //                 property:'opacity'
+    //             }
+    //         ]
+    //     }
+    // ]
     return (
         <div className="AboutSection">
             <hr />
@@ -80,19 +231,18 @@ export default function AboutSection() {
 
                 <main>
                     <div className="about-text">
-                    <Plx className="parallax-top" parallaxData={ parallaxData0 } >
-                        <img src={IconCss} alt="" />
 
-                    </Plx>
-                    <Plx className="parallax-top" parallaxData={ parallaxData1 } >
-                        <img src={IconDj} alt="" />
-
-                    </Plx>
+                    {parallaxDataTop.map((data,idx) => <Plx className="parallax-top" parallaxData={ data } >
+                        <img src={iconTop[idx]} alt="" />
+                    </Plx>)}
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur amet perspiciatis velit repellendus, est distinctio eaque asperiores nisi, quisquam consequuntur, facere numquam libero tempore! Amet repellat eos quos odio aspernatur.</p>
 
+
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur amet perspiciatis velit repellendus, est distinctio eaque asperiores nisi, quisquam consequuntur, facere numquam libero tempore! Amet repellat eos quos odio aspernatur.</p>
                             <div className="halfpoint">test</div>
-
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur amet perspiciatis velit repellendus, est distinctio eaque asperiores nisi, quisquam consequuntur, facere numquam libero tempore! Amet repellat eos quos odio aspernatur.</p>
+                        {parallaxDataBot.map((data,idx) => <Plx className="parallax-bot" parallaxData={ data } >
+                        <img src={iconBot[idx]} alt="" />
+                    </Plx>)}
                     </div>
                 </main>
                         <div className="endpoint" >test</div>
