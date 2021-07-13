b413b0eb8b29c4385c0d6d7740957a11a65f1734
onionsalot <trong.nguyen1008@gmail.com>
2021-07-13 07:31:34 -0400

    adds proportions for mobile view

--- src/Pages/IndexPage/IndexPage.jsx
+++ src/Pages/IndexPage/IndexPage.jsx
@@ -1,6 +1,7 @@
 import TopBanner from "../../Components/TopBanner/TopBanner";
 import AboutSection from "../../Components/AboutSection/AboutSection";
 import TechSection from "../../Components/TechSection/TechSection";
+import ProjectSection from "../../Components/ProjectSection/ProjectSection";
 import './IndexPage.scss';
 
 export default function IndexPage() {
@@ -12,6 +13,7 @@ export default function IndexPage() {
 
             <AboutSection />
             <TechSection />
+            <ProjectSection />
             <div style={{height: 5000}}/>
         </div>
     );
