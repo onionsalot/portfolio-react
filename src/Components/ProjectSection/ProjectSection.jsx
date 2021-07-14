ecade8f1f4a07047c1413279d97b417688b8d1fb
onionsalot <trong.nguyen1008@gmail.com>
2021-07-14 12:47:11 -0400

    adds projects page

--- src/Components/ProjectSection/ProjectSection.jsx
+++ src/Components/ProjectSection/ProjectSection.jsx
@@ -1,8 +1,29 @@
 import { useEffect, useState } from "react";
+import project1 from "../../images/sample-mp4-file.mp4"
+import project2 from "../../images/sample-mp4-file.mp4"
+import project3 from "../../images/sample-mp4-file.mp4"
+import project4 from "../../images/sample-mp4-file.mp4"
+import ProjectItem from "../ProjectItem/ProjectItem"
 import "./ProjectSection.scss";
 
 
 export default function ProjectSection() {
+  const [hover, setHover]= useState("");
+  const projectList = [
+    { "project1": project1},
+    { "project2": project2},
+    { "project3": project3},
+    { "project4": project4},
+  ]
+  function onMouseEnter(e) {
+    e.target.play()
+    setHover(e.target.id)
+  }
+  function onMouseLeave(e) {
+    e.target.pause()
+    e.target.currentTime=0;
+    setHover("")
+  }
 
   return (
     <div className="ProjectSection">
@@ -16,6 +37,10 @@ export default function ProjectSection() {
       </div>
 
       <main>
+        {projectList.map((project, idx)=> 
+          <ProjectItem idx={idx} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} name={Object.keys(project)[0]} video={Object.values(project)[0]} hover={hover} />
+        )}
+
 
       </main>
     </div>
