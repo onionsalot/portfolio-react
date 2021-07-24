cc0a4413dc9d3b4cc6292f0cbc914306475aa380
onionsalot <trong.nguyen1008@gmail.com>
2021-07-24 16:00:51 -0400

    imports partially finished top banner image and adds appropriate cropping

--- src/Components/TopBanner/TopBanner.jsx
+++ src/Components/TopBanner/TopBanner.jsx
@@ -1,7 +1,5 @@
 import { Parallax } from 'react-parallax';
-import foreground from '../../images/foreground.png';
-import middle from '../../images/middle.png';
-import background from '../../images/background.png';
+import background from '../../images/bloop.png';
 import { WindupChildren } from "windups";
 import './TopBanner.scss'
 
@@ -10,20 +8,17 @@ export default function TopBanner() {
     return (
         <div className="TopBanner">
             
-            <div className="opening-text">
+            {/* <div className="opening-text">
                 <WindupChildren>
                 {"This is "}
             <em>{"super"}</em>
             {" cool!"}
                 </WindupChildren>
-            </div>
-            <Parallax bgImage={background} strength={100} bgImageStyle={{height: "100vh", width:"100vw"}}>
-                <Parallax bgImage={middle} strength={0} bgImageStyle={{height: "100vh", width:"100vw", position:"absolute", bottom:0}} >
-                        <Parallax bgImage={foreground} strength={-300} bgImageStyle={{height: "100vh", width:"100vw", position:"absolute", bottom:-290}} >
-                            <div style={{ height: "100vh" }} />
-                        </Parallax>
-                </Parallax>
+            </div> */}
+            <Parallax blur={{ min: -15, max: 15 }} bgImage={background} strength={100} bgImageStyle={{height: "100vh"}}>
+            <div style={{ height: "1080px" }} />
             </Parallax>
+
         </div>
     );
 }
